7c93df1ab95a4f9e80bb1e2c0823251f670e0283
chloe0524 <chloco31@gmail.com>
2024-04-15 17:28:33 +0200

    removed handling of error

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,30 +11,27 @@ def hell_api():
     url_users = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
 
     response = requests.get(url_users)
-    if (response.ok):
-        jData = json.loads(response.content)
-        EMPLOYEE_NAME = jData["name"]
-    else:
-        response.raise_for_status()
+    jData = json.loads(response.content)
+    EMPLOYEE_NAME = jData.get("name")
 
     url_todos = "https://jsonplaceholder.typicode.com/todos"
     query = {'userId': sys.argv[1]}
 
     response = requests.get(url_todos, params=query)
-    if (response.ok):
-        jData = json.loads(response.content)
-        TOTAL_NUMBER_OF_TASKS = len(jData)
+    jData = json.loads(response.content)
+    TOTAL_NUMBER_OF_TASKS = len(jData)
 
-        NUMBER_OF_DONE_TASKS = [todo for todo in jData if todo
-                                ["completed"] is True]
+    NUMBER_OF_DONE_TASKS = 0
+    for todo in jData:
+        if todo.get("completed") is True:
+            NUMBER_OF_DONE_TASKS += 1
 
-        print("Employee " + EMPLOYEE_NAME + " is done with tasks(" +
-              str(len(NUMBER_OF_DONE_TASKS)) +
-              "/" + str(TOTAL_NUMBER_OF_TASKS) + ")")
-        for todo in NUMBER_OF_DONE_TASKS:
-            print("\t " + todo["title"])
-    else:
-        response.raise_for_status()
+    print("Employee " + str(EMPLOYEE_NAME) + " is done with tasks(" +
+          str(NUMBER_OF_DONE_TASKS) +
+          "/" + str(TOTAL_NUMBER_OF_TASKS) + ")")
+    for todo in jData:
+        if todo.get("completed") is True:
+            print("\t " + todo.get("title"))
 
 
 if __name__ == "__main__":
