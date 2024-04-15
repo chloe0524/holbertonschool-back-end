38c248a51eb9a7be92bc972b0f84c1f2bee2c47d
chloe0524 <chloco31@gmail.com>
2024-04-15 16:46:14 +0200

    repush without debug infos

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,30 +5,14 @@ import json
 import requests
 import sys
 
-debug = False
-
-if debug:
-    print("Param : " + sys.argv[1])
-    print("--------------------------------------------------")
-
 url_users = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
-
 
 response = requests.get(url_users)
 if (response.ok):
     jData = json.loads(response.content)
     name = jData["name"]
-
-    if debug:
-        print(response)
-        print("--------------------------------------------------")
-        print(response.json())
-        print("--------------------------------------------------")
-        print("The response contains {0} properties".format(len(jData)))
-        print(name)
 else:
     response.raise_for_status()
-
 
 url_todos = "https://jsonplaceholder.typicode.com/todos"
 query = {'userId': sys.argv[1]}
@@ -37,14 +21,6 @@ response = requests.get(url_todos, params=query)
 if (response.ok):
     jData = json.loads(response.content)
     nb_tasks = len(jData)
-
-    if debug:
-        print("\nTodos =================================="
-              "==============================")
-        print("--------------------------------------------------")
-        print("The response contains {0} properties".format(len(jData)))
-        print("Nb tasks=" + str(nb_tasks))
-        print("--------------------------------------------------")
 
     completed = 0
     for todo in jData:
