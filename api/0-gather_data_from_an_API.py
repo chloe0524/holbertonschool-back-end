ab9d899e198124dbd547a5f5cab2dc7fef5ed585
chloe0524 <chloco31@gmail.com>
2024-04-15 17:32:18 +0200

    removed handling of error

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,24 +11,30 @@ if __name__ == "__main__":
     url_users = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
 
     response = requests.get(url_users)
-    jData = json.loads(response.content)
-    EMPLOYEE_NAME = jData.get("name")
+    if (response.ok):
+        jData = json.loads(response.content)
+        EMPLOYEE_NAME = jData["name"]
+    else:
+        response.raise_for_status()
 
     url_todos = "https://jsonplaceholder.typicode.com/todos"
     query = {'userId': sys.argv[1]}
 
     response = requests.get(url_todos, params=query)
-    jData = json.loads(response.content)
-    TOTAL_NUMBER_OF_TASKS = len(jData)
+    if (response.ok):
+        jData = json.loads(response.content)
+        TOTAL_NUMBER_OF_TASKS = len(jData)
 
-    NUMBER_OF_DONE_TASKS = 0
-    for todo in jData:
-        if todo.get("completed") is True:
-            NUMBER_OF_DONE_TASKS += 1
+        NUMBER_OF_DONE_TASKS = 0
+        for todo in jData:
+            if todo["completed"] is True:
+                NUMBER_OF_DONE_TASKS += 1
 
-    print("Employee " + str(EMPLOYEE_NAME) + " is done with tasks(" +
-          str(NUMBER_OF_DONE_TASKS) +
-          "/" + str(TOTAL_NUMBER_OF_TASKS) + ")")
-    for todo in jData:
-        if todo.get("completed") is True:
-            print("\t " + todo.get("title"))
+        print("Employee " + EMPLOYEE_NAME + " is done with tasks(" +
+              str(NUMBER_OF_DONE_TASKS) +
+              "/" + str(TOTAL_NUMBER_OF_TASKS) + ")")
+        for todo in jData:
+            if todo["completed"] is True:
+                print("\t " + todo["title"])
+    else:
+        response.raise_for_status()
