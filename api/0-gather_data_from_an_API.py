cead614a691134d3bd182cfe74e4e6f703c920e0
chloe0524 <chloco31@gmail.com>
2024-04-15 19:45:41 +0200

    test push

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,47 +1,28 @@
 #!/usr/bin/python3
-"""task:0 gather data from an API"""
-
+"""
+Script that, using REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
 import requests
-import sys
-
+from sys import argv
 
 if __name__ == "__main__":
-    """ahfjhwdf"""
-    if len(sys.argv) < 2:
-        print("wrong")
-        sys.exit(1)
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    response = requests.get(url)
+    user = response.json()
+    EMPLOYEE_NAME = user.get("name")
 
-    user_id = sys.argv[1]
-    url_users = 'https://jsonplaceholder.typicode.com/users/' + user_id
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        user_id)
+    response = requests.get(url)
+    todos = response.json()
 
-    try:
-        response = requests.get(url_users)
-        response.raise_for_status()
-        user = response.json()
-        EMPLOYEE_NAME = user["name"]
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
-        sys.exit(1)
+    TOTAL_NUMBER_OF_TASKS = len(todos)
+    NUMBER_OF_DONE_TASKS = [task for task
+                            in todos if task.get("completed") is True]
 
-    url_todos = "https://jsonplaceholder.typicode.com/todos"
-    query = {'userId': user_id}
-
-    try:
-        response = requests.get(url_todos, params=query)
-        response.raise_for_status()
-        todos = response.json()
-        TOTAL_NUMBER_OF_TASKS = len(todos)
-
-        NUMBER_OF_DONE_TASKS = 0
-        for todo in todos:
-            if todo["completed"] is True:
-                NUMBER_OF_DONE_TASKS += 1
-
-        print("Employee {} is done with tasks({}/{})".format(
-            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-        for todo in todos:
-            if todo["completed"] is True:
-                print("\t {}".format(todo["title"]))
-    except requests.exceptions.RequestException as e:
-        print("{}".format(e))
-        sys.exit(1)
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, len(NUMBER_OF_DONE_TASKS), TOTAL_NUMBER_OF_TASKS))
+    for task in NUMBER_OF_DONE_TASKS:
+        print("\t {}".format(task.get("title")))
